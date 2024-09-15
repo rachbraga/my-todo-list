dae5df92e8e44597a41bbd3a7b9f7406d8321641
Rachel Braga <rachel.braga@flipsaude.com.br>
2024-09-15 16:24:49 -0300

    new: Modificação do App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { GlobalStyles } from './styles/GlobalStyles';
 
-function App() {
+import styled from 'styled-components';
+import TaskList from './components/TaskList';
+
+const Container = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100vh;
+background-color: #e0f7fa;
+`;
+
+const AppContainer = styled.div`
+width: 100%;
+max-width: 600px;
+padding: 2rem;
+background-color: #f9f9f9;
+border-radius: 15px;
+box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+`;
+
+const Title = styled.h1`
+text-align: center;
+margin-bottom: 1rem;
+color: #ff66b2;
+`;
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyles />
+      <Container>
+        <AppContainer>
+          <Title>MY TO-DO LIST</Title>
+          <TaskList />
+        </AppContainer>
+      </Container>
+    </>
   );
-}
+};
 
 export default App;
